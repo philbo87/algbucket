3e91a4daef11484dd2ae5607f25d65ecbd5de767
Phil Busch <phil.busch@gmail.com>
2015-08-05 20:16:29 -0500

    Some Lowest Common Ancestor logic

--- com/algorithms/datastructures/tree/Tree.java
+++ com/algorithms/datastructures/tree/Tree.java
@@ -4,27 +4,44 @@ public class Tree {
 
 	public static void main(String[] args){
 			
-		TreeNode twenty = new TreeNode(20);//Root
-		TreeNode ten = new TreeNode(10);
-		TreeNode five = new TreeNode(5);
-		TreeNode three = new TreeNode(3);
-		TreeNode seven = new TreeNode(7);
-		TreeNode fifteen = new TreeNode(15);
-		TreeNode seventeen = new TreeNode(17);
-		TreeNode thirty = new TreeNode(30);
+//		TreeNode twenty = new TreeNode(20);//Root
+//		TreeNode ten = new TreeNode(10);
+//		TreeNode five = new TreeNode(5);
+//		TreeNode three = new TreeNode(3);
+//		TreeNode seven = new TreeNode(7);
+//		TreeNode fifteen = new TreeNode(15);
+//		TreeNode seventeen = new TreeNode(17);
+//		TreeNode thirty = new TreeNode(30);
+//		
+//		five.setLeft(three);
+//		five.setRight(seven);
+//		
+//		fifteen.setRight(seventeen);
+//		
+//		ten.setLeft(five);
+//		ten.setRight(fifteen);
+//		
+//		twenty.setLeft(ten);
+//		twenty.setRight(thirty);
 		
-		five.setLeft(three);
-		five.setRight(seven);
+		//Simple tree
+		//      99
+		//      / \
+		//     100 101
+		//		/\
+		//	102	 103
+		TreeNode n99 = new TreeNode(99);
+		TreeNode n100 = new TreeNode(100);
+		TreeNode n101 = new TreeNode(101);
+		TreeNode n102 = new TreeNode(102);
+		TreeNode n103 = new TreeNode(103);
 		
-		fifteen.setRight(seventeen);
-		
-		ten.setLeft(five);
-		ten.setRight(fifteen);
-		
-		twenty.setLeft(ten);
-		twenty.setRight(thirty);
-		
-		System.out.print("isBST: " + isBST(twenty));
+		n99.setLeft(n100);
+		n99.setRight(n101);
+		n100.setLeft(n102);
+		n100.setRight(n103);
+//		System.out.println("isBST: " + isBST(twenty));
+		System.out.println("LCA " + findCommonAncestor(n99,n102,n103).getV());
 	}
 	
 	
@@ -48,5 +65,45 @@ public class Tree {
 		
 		return true;
 	}
-
+	
+	/**
+	 * Root changes as recursion occurs. n1/n2 do not
+	 * 
+	 * Runtime O(n)
+	 * http://www.geeksforgeeks.org/lowest-common-ancestor-binary-tree-set-1/
+	 * @param root
+	 * @param n1
+	 * @param n2
+	 * @return
+	 */
+	public static TreeNode findCommonAncestor(TreeNode root, TreeNode n1, TreeNode n2)
+	{
+		/*Special Case 1*/
+		//If the passed in root is null, the parent node had no left/right child to pass in
+		if(root== null){
+			return null;
+		}
+		
+		/*Special Case 2*/
+		//If the root is the same value as n1 or n2, we are on that level in the tree of one of our target values. 
+		//Stop going deeper, return this root
+		if(root.getV() == n1.getV() || root.getV() == n2.getV()){
+			return root;
+		}
+		
+		//The recursive calls to find the common ancestor. Could be null if there is no left/right children
+		TreeNode leftCA = findCommonAncestor(root.getLeft(), n1,n2);
+		TreeNode rightCA = findCommonAncestor(root.getRight(),n1,n2);
+		
+		//When leftCA and rightCA are not null, then we've found the common ancestor, return the current value of root
+		if(leftCA != null && rightCA!=null){
+			return root;
+		}
+		
+		//If only one of these is not null, then it is the common ancestor. Note they could both also be null and thus we'd return null.
+		if(leftCA != null) 
+			return leftCA;
+		else 
+			return rightCA;
+	}
 }
