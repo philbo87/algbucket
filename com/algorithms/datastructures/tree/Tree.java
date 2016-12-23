4adc2e51f347239db1b7093b89fc0e2a7cfc0ae9
Phil Busch <phil.busch@gmail.com>
2016-12-23 10:13:33 -0600

    Added pre/post/in order traversal

--- com/algorithms/datastructures/tree/Tree.java
+++ com/algorithms/datastructures/tree/Tree.java
@@ -40,11 +40,45 @@ public class Tree {
 		n99.setRight(n101);
 		n100.setLeft(n102);
 		n100.setRight(n103);
+		System.out.println("Pre-Order Traversal");
+		preOrder(n99);
+		
+		System.out.println("In order traversal:");
+		inOrder(n99);
+		
+		System.out.println("Post order traversal:");
+		postOrder(n99);
+		
 //		System.out.println("isBST: " + isBST(twenty));
-		System.out.println("LCA " + findCommonAncestor(n99,n102,n103).getV());
+//		System.out.println("LCA " + findCommonAncestor(n99,n102,n103).getV());
 	}
 	
+	public static void preOrder(TreeNode node){
+		if(node != null){
+			printNode(node);
+			preOrder(node.getLeft());
+			preOrder(node.getRight());
+		}
+	}
 	
+	public static void inOrder(TreeNode node){
+		if(node != null){
+			inOrder(node.getLeft());
+			printNode(node);
+			inOrder(node.getRight());
+		}
+	}
+	
+	public static void postOrder(TreeNode node){
+		if(node != null){
+			postOrder(node.getLeft());
+			postOrder(node.getRight());
+			printNode(node);
+		}
+	}
+	public static void printNode(TreeNode node){
+		System.out.println(node.getV());
+	}
 	/**
 	 * Checks to see if this is a Binary Search Tree. Uses an In order Traversal. Takes O(N) time since you've gotta visit
 	 * all the nodes
