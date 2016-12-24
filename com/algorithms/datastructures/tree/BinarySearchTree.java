26d49769345bc175a7d8233a3534e8cbc760c164
Phil Busch <phil.busch@gmail.com>
2016-12-24 17:17:07 -0600

    Implemented binary tree delete

--- com/algorithms/datastructures/tree/BinarySearchTree.java
+++ com/algorithms/datastructures/tree/BinarySearchTree.java
@@ -6,64 +6,174 @@ public class BinarySearchTree extends Tree {
 	private static TreeNode root = null;
 
 	public static void main(String[] args) {
-		insert(new TreeNode(3), root);
-		insert(new TreeNode(1), root);
-		insert(new TreeNode(5), root);
-		insert(new TreeNode(7), root);
-		insert(new TreeNode(95), root);
-		inOrder(root);
+		// insert(new TreeNode(3), root);
+		// insert(new TreeNode(1), root);
+		// insert(new TreeNode(5), root);
+		// insert(new TreeNode(7), root);
+		// insert(new TreeNode(95), root);
+		// inOrder(root);
+		//
+		// printNode(max(root));
+		// printNode(min(root));
+		//
+		// root = null;
+		// insert(new TreeNode(300), root);
+		// insert(new TreeNode(299), root);
+		// insert(new TreeNode(298), root);
+		// TreeNode two97 = new TreeNode(297);
+		// insert(two97, root);
+		// TreeNode last = new TreeNode(296);
+		// insert(last, root);
+		// printNode(successor(last));
+		// printNode(predecessor(two97));
+		// printNode(predecessor(last));
+		//
+		// root = null;
+		// insert(new TreeNode(300), root);
+		// insert(new TreeNode(200), root);
+		// insert(new TreeNode(100), root);
+		// insert(new TreeNode(101), root);
+		// TreeNode last2 = new TreeNode(102);
+		// insert(last2, root);
+		// printNode(successor(last2));
+		// printNode(predecessor(last2));
 
-		printNode(max(root));
-		printNode(min(root));
-		
 		root = null;
-		insert(new TreeNode(300), root);
-		insert(new TreeNode(299), root);
-		insert(new TreeNode(298), root);
-		TreeNode two97 = new TreeNode(297);
-		insert(two97, root);
-		TreeNode last = new TreeNode(296);
-		insert(last, root);
-		printNode(successor(last));
-		printNode(predecessor(two97));
-		printNode(predecessor(last));
-		
+		TreeNode firstNode = new TreeNode(105);
+		insert(firstNode, root);
+		TreeNode secondNode = new TreeNode(75);
+		insert(secondNode, root);
+		TreeNode thirdNode = new TreeNode(205);
+		insert(thirdNode, root);
+		TreeNode fourthNode = new TreeNode(315);
+		insert(fourthNode, root);
+		TreeNode fifthNode = new TreeNode(170);
+		insert(fifthNode, root);
+		TreeNode sixthNode = new TreeNode(160);
+		insert(sixthNode, root);
+		TreeNode seventhNode = new TreeNode(175);
+		insert(seventhNode, root);
+		delete(firstNode);
+
 		root = null;
-		insert(new TreeNode(300), root);
-		insert(new TreeNode(200), root);
-		insert(new TreeNode(100), root);
-		insert(new TreeNode(101), root);
-		TreeNode last2 = new TreeNode(102);
-		insert(last2, root);
-		printNode(successor(last2));
-		printNode(predecessor(last2));
+		insert(new TreeNode(15), root);
+		TreeNode z = new TreeNode(5);
+		insert(z, root);
+		insert(new TreeNode(3), root);
+		insert(new TreeNode(12), root);
+		insert(new TreeNode(10), root);
+		insert(new TreeNode(6), root);
+		insert(new TreeNode(7), root);
+		insert(new TreeNode(13), root);
+		insert(new TreeNode(16), root);
+		insert(new TreeNode(20), root);
+		insert(new TreeNode(18), root);
+		insert(new TreeNode(23), root);
+		delete(z);
+		inOrder(root);
 	}
 
-	//Get successor node O(h)
-	//Successor is the next largest node
+	// O(h)
+	private static void delete(TreeNode node) {
+		if (node.getLeft() == null && node.getRight() == null) {
+			// No children --> Just delete this
+			// Figure out if this is left or right child of parent
+			if (node.getParent() == null) {
+				// This is the root, null the root
+				root = null;
+			} else if (node.getParent().getLeft() != null && node.getParent().getLeft().equals(node)) {
+				node.getParent().setLeft(null);
+			} else if (node.getParent().getRight() != null && node.getParent().getRight().equals(node)) {
+				node.getParent().setRight(null);
+			}
+
+			node.setParent(null);
+
+		} else if (node.getLeft() != null && node.getRight() == null) {
+			// One child, and it is the left node. Just need to move pointers to
+			// remove this node and point parent/child at each other
+			TreeNode child = node.getLeft();
+			TreeNode parent = node.getParent();
+			child.setParent(parent);
+
+			if (parent != null) {
+				if (parent.getRight() != null && parent.getRight().equals(node)) {
+					// The removed node is the right child of the parent, set
+					// the removed child to be the parent's right child
+					parent.setRight(child);
+				}else{
+					parent.setLeft(child);
+				}
+			} else {
+				// This was root
+				root = child;
+			}
+
+			// Not sure if this is really necessary
+			node.setLeft(null);
+			node.setParent(null);
+		} else if (node.getRight() != null && node.getLeft() == null) {
+			// One child, and it is the right node
+			TreeNode child = node.getRight();
+			TreeNode parent = node.getParent();
+			child.setParent(parent);
+
+			if (parent != null) {
+				if (parent.getRight() != null && parent.getRight().equals(node)) {
+					// The removed node is the right child of the parent, set
+					// the removed child to be the parent's right child
+					parent.setRight(child);
+				}else{
+					parent.setLeft(child);
+				}
+			} else {
+				root = child;
+			}
+
+			// Not sure if this is really necessary
+			node.setRight(null);
+			node.setParent(null);
+		} else {
+			// Node has a L and R child
+			// Worst case. Find successor y of node x,
+			// Set value of x to y.val
+			// Delete y, keeping in mind it could have right subtree and be in
+			// same scenario
+			// No need to change root pointer in this scenario as we are only
+			// manipulating values
+			TreeNode successor = successor(node);
+			node.setV(successor.getV());
+			delete(successor);
+		}
+	}
+
+	// Get successor node O(h)
+	// Successor is the next largest node
 	private static TreeNode successor(TreeNode node) {
 		if (node.getRight() != null) {
 			return min(node.getRight());
 		} else {
 			TreeNode parent = node.getParent();
-			while (parent != null && parent.getParent() != null && parent.getLeft() == null && parent.getRight() != null) {
+			while (parent != null && parent.getParent() != null && parent.getLeft() == null
+					&& parent.getRight() != null) {
 				parent = parent.getParent();
 			}
 			return parent;
 		}
 	}
-	
-	//Get predecessor node O(h)
-	//Predecessor is the next smallest node
-	private static TreeNode predecessor(TreeNode node){
-		if(node.getLeft() != null){
+
+	// Get predecessor node O(h)
+	// Predecessor is the next smallest node
+	private static TreeNode predecessor(TreeNode node) {
+		if (node.getLeft() != null) {
 			return max(node.getLeft());
-		}else{
+		} else {
 			TreeNode parent = node.getParent();
-			while(parent != null && parent.getParent() != null && parent.getRight() == null && parent.getLeft() != null){
+			while (parent != null && parent.getParent() != null && parent.getRight() == null
+					&& parent.getLeft() != null) {
 				parent = parent.getParent();
 			}
-			
+
 			return parent;
 		}
 	}
